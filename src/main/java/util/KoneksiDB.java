0472e1cbff4d2c4ac78b00720cbf994504bd7568
Xiaomao15 <kagayaku15h@gmail.com>
2025-12-21 05:46:47 +0700

    update Koneksi

--- src/main/java/util/KoneksiDB.java
+++ src/main/java/util/KoneksiDB.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author MyBook Hype AMD
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class KoneksiDB {
-    
+
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/dbpemesanan_kantin";
+            String user = "postgres";
+            String pass = "12345";
+
+            return DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace(); // WAJIB
+            System.out.println("Koneksi gagal: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        if (KoneksiDB.getConnection() != null) {
+            System.out.println("Koneksi berhasil");
+        }
+    }
 }
